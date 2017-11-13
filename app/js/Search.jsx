5c896da5f63475beebc9f188af7a4d6a40f62ef5
Elise Linn <elise_81@yahoo.com>
2017-11-13 14:11:15 -0800

    map the data to <h3>s displaying the title of each show

--- app/js/Search.jsx
+++ app/js/Search.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import preload from '../data.json';
+import data from '../data.json';
 
 const Search = () => (
     <div className='search'>
-        <pre>
-            <code>{JSON.stringify(preload, null, 4)}</code
-        ></pre>
+        { data.shows.map(show => <h3>{ show.title }</h3>) }
     </div>
 );
 
