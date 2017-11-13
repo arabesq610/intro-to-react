2f975cf280f94763ef76d42d07ebb680fc653b79
Elise Linn <elise_81@yahoo.com>
2017-11-13 13:18:01 -0800

    formatting

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,11 +1,11 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
     context: __dirname,
     entry: "./js/ClientApp.jsx",
     devtool: "cheap-eval-source-map", // inline all source maps into code, only in dev,
     output: {
-        path: path.join(__dirname, 'public'),
+        path: path.join(__dirname, 'public'), // always has to be an absolute path
         filename: 'bundle.js'
     },
     devServer: {
@@ -27,7 +27,6 @@ module.exports = {
                 test: /\.jsx?$/,
                 loader: 'eslint-loader',
                 exclude: /node_modules/
-
             },
             {
                 test: /\.jsx?$/, // run any file with ext of either js or jsx through babel
@@ -35,4 +34,4 @@ module.exports = {
             }
         ]
     }
-}
+};
