2cd140b4f403af261981622dda028df5b08c8cef
Elise Linn <elise_81@yahoo.com>
2017-11-14 11:38:50 -0800

    add styled components

--- app/js/ShowCard.jsx
+++ app/js/ShowCard.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+// using tagged (styled), gets fed into styled-components template literal syntax
+const Wrapper = styled.div`
+    width: 32%;
+    border: 2px solid #333;
+    border-radius: 4px;
+    margin-bottom: 25px;
+    padding-right: 10px;
+    overflow: hidden;
+`;
+
+const Image = styled.img`
+    width: 46%;
+    float: left;
+    margin-right: 10px;
+`;
 
 const ShowCard = props => (
-    <div className="show-card">
-        <img alt={`${ props.title } Poster`} src={`/public/img/posters/${ props.poster }`} />
+    <Wrapper>
+        <Image alt={`${ props.title } Poster`} src={`/public/img/posters/${ props.poster }`} />
         <div>
             <h3>{ props.title }</h3>
             <h4>({ props.year })</h4>
             <p>{ props.description}</p>
         </div>
-    </div>
+    </Wrapper>
 );
 
 ShowCard.propTypes = {
